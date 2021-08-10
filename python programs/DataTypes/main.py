de4745978efa4a45b509194d7a004216bbd11f3f
venkatamamidibathula <venkata.mamidibathula@outlook.com>
2021-08-09 20:39:13 -0700

    added new notes

--- python programs/DataTypes/main.py
+++ python programs/DataTypes/main.py
@@ -1,5 +1,7 @@
-prices_lookup={'Apples':456,'Oranges':532,'Milk':960}
-print(prices_lookup.values())
-print(prices_lookup.keys())
-p=prices_lookup['Apples']
-print(p)
+t=set()
+t.add(9)
+t.add(10)
+t.add(11)
+
+print(t)
+
